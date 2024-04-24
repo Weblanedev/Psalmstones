fbb89386f999f2b5abad926e3bc13c68b183a9d4
Martins Godwin Ikpe <75488009+b0yw0nder3100@users.noreply.github.com>
2024-04-24 07:59:23 +0100

    Update page.js

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -180,6 +180,17 @@ const Contact = () => {
               </p>
             </div>
           </div>
+                            <div className="flex gap-5 items-start justify-center">
+            <img src="/assets/phone.png" alt="" className="w-6 pt-1" />
+            <div className="flex flex-col items-start justify-center gap-3">
+              <h4 className="text-[24px] leading-[24px] font-medium">
+                Address
+              </h4>
+              <p className="text-yellow-500 leading-normal">
+35 Kola Amodu Crescent, Magodo, Ikosi Ketu, Lagos
+              </p>
+            </div>
+          </div>
         </div>
       </section>
 
